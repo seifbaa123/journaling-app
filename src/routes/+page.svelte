d1c070d72df568feb968d08ba17cf7483f99fcab
seif <seifbaa123@gmail.com>
2023-10-17 07:58:24 +0100

    list journals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import Calendar from '$lib/components/calendar/Calendar.svelte';
 	import { formatDate } from '$lib/utils.js';
+	import JournalsList from './JournalsList.svelte';
 
 	export let data;
 </script>
@@ -9,11 +10,24 @@
 <a href="/heatmap">heatmap</a>
 
 <div class="container">
-	<Calendar journals={data.journals} onDateSelected={(date) => goto(`/${formatDate(date)}`)} />
+	<div class="item center">
+		<Calendar journals={data.journals} onDateSelected={(date) => goto(`/${formatDate(date)}`)} />
+	</div>
+	<div class="item">
+		<JournalsList journals={data.journals} />
+	</div>
 </div>
 
 <style>
 	.container {
+		display: flex;
+	}
+
+	.item {
+		flex: 1;
+	}
+
+	.center {
 		width: 100%;
 		min-height: 100vh;
 		display: flex;
---
+++ src/routes/JournalsList.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Journals } from '@prisma/client';
+
+	export let journals: Journals[];
+</script>
+
+<div class="journals">
+	{#each journals as j}
+		<div class="journal">
+			{#if j.title}
+				<a href={j.date}>{j.title}</a>
+			{:else}
+				<a href={j.date}><i>Empty title</i></a>
+			{/if}
+			<span>{j.date}</span>
+			<p>{j.body}</p>
+		</div>
+	{/each}
+</div>
+
+<style>
+	a {
+		display: block;
+		font-weight: bold;
+		font-size: 1.25rem;
+	}
+</style>
