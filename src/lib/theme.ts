7b4db046b2adb335598ce35ba8a6d52cb46ebdf6
seif <seifbaa123@gmail.com>
2023-12-06 09:56:05 +0100

    add loading animation

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,18 +1,18 @@
-import { browser } from "$app/environment"
+import { browser } from '$app/environment';
 
 export function getTheme(): boolean {
-    if (!browser) return true
-    const darkTheme = localStorage.getItem("theme") === "dark"
-    setTheme(darkTheme)
-    return darkTheme
+	if (!browser) return true;
+	const darkTheme = localStorage.getItem('theme') === 'dark';
+	setTheme(darkTheme);
+	return darkTheme;
 }
 
 export function setTheme(isDarkMode: boolean) {
-    if (isDarkMode) {
-        document.body.classList.add('dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.body.classList.remove('dark');
-        localStorage.setItem('theme', 'light');
-    }
+	if (isDarkMode) {
+		document.body.classList.add('dark');
+		localStorage.setItem('theme', 'dark');
+	} else {
+		document.body.classList.remove('dark');
+		localStorage.setItem('theme', 'light');
+	}
 }
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let showLoadingContainer = true;
+
+	setTimeout(() => {
+		showLoadingContainer = false;
+	}, 1000);
+</script>
+
+{#if showLoadingContainer}
+	<div class="loading-container">
+		<div class="loading-dot" />
+		<div class="loading-dot" />
+		<div class="loading-dot" />
+	</div>
+{/if}
+
+<slot />
